f2c11568876da6aceecbed45ac106eee4eb6e37b
Diego Ms <diego.mtzs00@gmail.com>
2022-04-03 22:41:27 -0500

    Update routes.py
    Added abort option if the load of the map from the side of python fails

--- Server/app/routes.py
+++ Server/app/routes.py
@@ -16,15 +16,18 @@ def dateNow():
 # -------------Endpoints-------------
 @app.route("/", methods=["GET"])# Mapa de google maps para las coordenadas
 def my_map():
-    mymap = Map(identifier="view-center",
-    varname="mymap",
-    style="height:1520px;width:1900px;margin:0;",
-    lat=19.362534,
-    lng=-99.249061,
-    zoom=19,
-    markers=[(19.362534, -99.249061)])
+    try:
+        mymap = Map(identifier="view-center",
+        varname="mymap",
+        style="height:1520px;width:1900px;margin:0;",
+        lat=19.362534,
+        lng=-99.249061,
+        zoom=19,
+        markers=[(19.362534, -99.249061)])
 
-    return render_template("googlemap.html", pageTitle= "Map", mymap=mymap)
+        return render_template("googlemap.html", pageTitle= "Map", mymap=mymap)
+    except Exception:
+        abort(500)
 
 @app.route("/sendemail", methods=["POST"])# Endpoint para mandar el correo que se llamará desde otro lado
 def send_email():
