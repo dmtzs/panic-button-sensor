8e06b53834aaabef47fd7e4df6a9cb49a4adefbe
Diego Ms <diego.mtzs00@gmail.com>
2022-04-03 22:35:37 -0500

    Creation and updates for google maps in flask server
    Creation and updates of files so I can be able to insert and calculate correctly all information needed to show a marker in a google map and also the map can be shown in the html template

--- Server/app/__init__.py
+++ Server/app/__init__.py
@@ -1,8 +1,13 @@
 try:
     from flask import Flask
+    from flask_googlemaps import GoogleMaps
 except ImportError as eImp:
     print(f"The following import ERROR occurred in {__file__}: {eImp}")
 
 app= Flask(__name__)
+
+app.config['GOOGLEMAPS_KEY'] = "AIzaSyA6TDHaUUCPJ0qn0TwB8If3HNsOKh-sPDU"# Se supone es la misma clave de API que me dan y que uso en el ESP32
+# app.config['GOOGLEMAPS_KEY'] = "8JZ7i18MjFuM35dJHq70n3Hx4"
+GoogleMaps(app)
 
 from app import routes, admin_routes, errorHandlers
--- Server/app/routes.py
+++ Server/app/routes.py
@@ -1,6 +1,7 @@
 try:
     from app import app
-    import datetime as dt
+    from flask_googlemaps import Map
+    from datetime import datetime as dt
     from flask import render_template, Response, request, abort
 except ImportError as eImp:
     print(f"The following import ERROR occurred in {__file__}: {eImp}")
@@ -9,13 +10,21 @@ except ImportError as eImp:
 @app.context_processor
 def dateNow():
     return {
-        "now": dt.datetime.now()
+        "now": dt.now()
     }
 
 # -------------Endpoints-------------
 @app.route("/", methods=["GET"])# Mapa de google maps para las coordenadas
-def index():
-    return render_template("googlemap.html", pageTitle= "Map")
+def my_map():
+    mymap = Map(identifier="view-center",
+    varname="mymap",
+    style="height:1520px;width:1900px;margin:0;",
+    lat=19.362534,
+    lng=-99.249061,
+    zoom=19,
+    markers=[(19.362534, -99.249061)])
+
+    return render_template("googlemap.html", pageTitle= "Map", mymap=mymap)
 
 @app.route("/sendemail", methods=["POST"])# Endpoint para mandar el correo que se llamará desde otro lado
 def send_email():
