cfc984fee2bbc7a86df5c47a907490a9fd5f8015
Diego Ms <diego.mtzs00@gmail.com>
2022-04-19 11:11:34 -0500

    Update runApp.py
    Change to the production server in the file

--- Server/runApp.py
+++ Server/runApp.py
@@ -7,12 +7,12 @@ except ImportError as eImp:
 if __name__== "__main__":
     try:
         # -----------------Dev mode-----------------
-        app.run(host= "127.0.0.1", port= 5000, debug= True)
+        # app.run(host= "127.0.0.1", port= 5000, debug= True)
         # debug= True for apply changes made into the files without restarting the flask server
 
         # -----------------Prod mode----------------
-        #appServer= WSGIServer(("127.0.0.1", 5000), app)
-        #appServer.serve_forever()
+        appServer= WSGIServer(("127.0.0.1", 5000), app)
+        appServer.serve_forever()
     except Exception as eImp:
         print(f"The following import ERROR occurred in {__file__}: {eImp}")
     finally:
