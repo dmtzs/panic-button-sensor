1de9be7a4d3cfe8e2ba7f39ea8cc2f68a916f652
Diego Ms <diego.mtzs00@gmail.com>
2022-04-21 22:04:52 -0500

    Update routes.py
    Added another endpoint just to send an email to the desired recipient

--- Server/app/routes.py
+++ Server/app/routes.py
@@ -1,8 +1,12 @@
 try:
+    import json
+    import smtplib
     from app import app
     from firebase_admin import db
     from flask_googlemaps import Map
     from datetime import datetime as dt
+    from email.mime.text import MIMEText
+    from email.mime.multipart import MIMEMultipart
     from flask import render_template, Response, abort
 except ImportError as eImp:
     print(f"The following import ERROR occurred in {__file__}: {eImp}")
@@ -16,6 +20,26 @@ def get_coordinates():
 
     print(result[result_key])
     return result[result_key]# Returns dicctionary {'latitude': 19.362534, 'longitude': -99.249061}
+
+# Method to send email
+def email_send(destiny_email):
+    #send email 
+    toaddr = destiny_email
+    # fromaddr = "mmicros717@gmail.com"
+    fromaddr = "eder1354684@gmail.com"
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = "Alarm geolocation"
+    body="--------------------------------------------------------------------------- \n" + "Go and visit the next IP address: http://137.184.131.234/" + "\n--------------------------------------------------------------------------- "
+    msg.attach(MIMEText(body, 'plain'))
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(fromaddr, "passwd")
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit()
+    print("Successfully sent mail ")
 
 # -------------Context processor-------------
 @app.context_processor
@@ -43,7 +67,21 @@ def my_map():
 
 @app.route("/sendemail", methods=["POST"])# Endpoint para mandar el correo que se llamará desde otro lado
 def send_email():
-    respCode = 200
-    respJSON = {}
+    try:
+        coordinates = get_coordinates()
+        email_send(coordinates["email-dest"])
+        respCode = 200
+        respJSON = {
+            "responseCode": respCode,
+            "responseMessage": "Email sent successfully"
+        }
+        respJSON = json.dumps(respJSON)
+    except Exception as ex:
+        respCode = 200
+        respJSON = {
+            "responseCode": respCode,
+            "responseMessage": str(ex)
+        }
+        respJSON = json.dumps(respJSON)
 
-    return Response(respJSON, mimetype="application/json"), respCode
+    return Response(respJSON, mimetype="application/json")
