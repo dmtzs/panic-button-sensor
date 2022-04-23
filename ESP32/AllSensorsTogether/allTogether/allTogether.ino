c43cbb309504805b79b06484fe2b533ea90449d4
Diego Ms <diego.mtzs00@gmail.com>
2022-04-22 20:23:10 -0500

    Update allTogether.ino
    Final implementation of this file, it works now as expected

--- ESP32/AllSensorsTogether/allTogether/allTogether.ino
+++ ESP32/AllSensorsTogether/allTogether/allTogether.ino
@@ -1,28 +1,38 @@
-// Bibliotecas nativas
+// Native libraries
 #include <Arduino.h>
 #include <WiFi.h>
 
-// Biblioteca que se debe de instalar
+// Libraries that should be installed
 #include <WifiLocation.h>
 #include <FirebaseESP32.h>
 
+//---------------------------------------Credentials---------------------------------------
 #define googleApiKey "googleApiKey"
 #define ssid "ssid"
 #define passwd "passwd"
 
-#define FIREBASE_HOST "firebaseURL"
-#define FIREBASE_Authorization_key "Firebase_authKey"
+#define FIREBASE_HOST "FIREBASE_HOST"
+#define FIREBASE_Authorization_key "FIREBASE_Authorization_key"
+// ----------------------------------------------------------------------------------------
 
-//Debajo para mq3
+//For mq3
 #define GAS_SENSOR A0
-int val, control_request=0;
-String lat, lng;
+int val, control_request=0, control_alarm_led=0;
+String lat, lng, time_stamp;
 
-//Firebase objetos
+//push button pins and out pins
+const int pushPanic = 15;
+const int pushLedAlarm = 19;
+const int ledRojo = 4;
+const int ledAzul = 5;
+const int alarma = 18;
+
+//Firebase objects
 FirebaseData firebaseData;
 FirebaseJson json;
 
 WifiLocation location (googleApiKey);
+String ESP_serie;
 
 // Set time via NTP, as required for x.509 validation
 void setClock () {
@@ -36,10 +46,15 @@ void setClock () {
         now = time (nullptr);
     }
     struct tm timeinfo;
+    struct tm timeinfo2;//temporal
+    localtime_r(&now, &timeinfo2);
+    Serial.println("Other time stamp");
+    Serial.print (asctime (&timeinfo2));
     gmtime_r (&now, &timeinfo);
     Serial.print ("\n");
     Serial.print ("Current time: ");
     Serial.print (asctime (&timeinfo));
+
 }
 
 void connectToWifi() {
@@ -77,23 +92,70 @@ void setup() {
     //Debajo de mq3
     delay(10);
     pinMode(GAS_SENSOR, INPUT);
+    pinMode(pushPanic, INPUT);
+    pinMode(pushLedAlarm, INPUT);
+
+    pinMode(ledRojo, OUTPUT);
+    pinMode(ledAzul, OUTPUT);
+    pinMode(alarma, OUTPUT);
+
+    digitalWrite(ledRojo, LOW);
+    digitalWrite(ledAzul, LOW);
+    digitalWrite(alarma, LOW);
+
+    Firebase.begin(FIREBASE_HOST,FIREBASE_Authorization_key);
+    ESP_serie = WiFi.macAddress();
+    // FirebaseJson json_value;// A variable to set the key and content I want
+    // json_value.set("email-dest", "mmicros717@gmail.com");
+    // Firebase.pushJSON(firebaseData, "/Coordinates/ESP_" + ESP_serie + "/lectura", json_value);
 }
 
 void loop() {
-    // String data = Serial.readStringUntil('\n');// En vez de esto será la lectura del sensor y/o push button
+    int pushPanicState = digitalRead(pushPanic);
+    int pushLedAlarmState = digitalRead(pushLedAlarm);
     val=analogRead(GAS_SENSOR);//Lectura sensor mq3 y debajo if de la lectura
+    Serial.print("MQ3: ");
     Serial.println(val);
-    if(val >= 1200 && control_request == 0){
+
+    if(val >= 1200 && control_request == 0) {
       Serial.println("Alcohol detected, sending geolocation");
       control_request=1;
       getLocation();
-      Firebase.begin(FIREBASE_HOST,FIREBASE_Authorization_key);
-      Firebase.setDouble(firebaseData, "/Coordinates/-N017k3urhCwePnWAAjX/latitude", lat.toFloat());
-      Firebase.setDouble(firebaseData, "/Coordinates/-N017k3urhCwePnWAAjX/longitude", lng.toFloat());
+      FirebaseJson json_value;// A variable to set the key and content I want
+      json_value.set("latitude", lat.toFloat());
+      json_value.set("longitude", lng.toFloat());
+
+      Firebase.pushJSON(firebaseData, "/Coordinates/ESP_" + ESP_serie + "/lectura", json_value);
+    }
+    else if(pushPanicState == HIGH && control_request == 0) {
+      Serial.println("Panic button detected");
+      control_request=1;
+      getLocation();
+      FirebaseJson json_value;// A variable to set the key and content I want
+      json_value.set("latitude", lat.toFloat());
+      json_value.set("longitude", lng.toFloat());
+
+      Firebase.pushJSON(firebaseData, "/Coordinates/ESP_" + ESP_serie + "/lectura", json_value);
+    }
+    else if(pushLedAlarmState == HIGH && control_alarm_led == 0) {
+      Serial.println("Encender alarma");
+      control_alarm_led = 1;
+      digitalWrite(ledRojo, HIGH);
+      digitalWrite(ledAzul, HIGH);
+      digitalWrite(alarma, HIGH);
+    }
+    else if(pushLedAlarmState == HIGH && control_alarm_led == 1) {
+      control_alarm_led = 0;
+      Serial.println("Apagar alarma");
+      digitalWrite(ledRojo, LOW);
+      digitalWrite(ledAzul, LOW);
+      digitalWrite(alarma, LOW);
     }
     else {
       control_request=0;
       Serial.println("No data received");
     }
-    delay(1000);
+    delay(2000);
 }
+
+// For arduino.json: ESP32\\AllSensorsTogether\\allTogether\\allTogether.ino
